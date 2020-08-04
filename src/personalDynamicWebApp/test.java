5e704252097150ae684afe0946a0a6661cc3111c
Hendrik <hendrik.wirthwein@gmail.com>
2020-08-04 17:32:59 +0200

    created personal dynamicWebApp as spring,maven app

---
+++ src/personalDynamicWebApp/test.java
@@ -0,0 +1,11 @@
+package personalDynamicWebApp;
+
+public class test{
+
+	public static void main(String [] args){
+
+  System.out.println("this file is to test stuff");
+  
+  }
+  
+}
--- test.java
+++
@@ -1,9 +0,0 @@
-public class test{
-
-public static void main(String [] args){
-
-  System.out.println("this file is to test stuff);
-  
-  }
-  
-  
