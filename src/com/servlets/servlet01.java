ab8047cb54efb2a2380c26e883a5bed22ea97335
Hendrik <hendrik.wirthwein@gmail.com>
2020-08-07 11:43:22 +0200

    logic happens in servlet and gets printed on page

--- src/com/servlets/servlet01.java
+++ src/com/servlets/servlet01.java
@@ -6,38 +6,30 @@ import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/add")
 public class servlet01 extends HttpServlet {
 	
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
-		Cookie kekse [] = req.getCookies(); // there are many cookies you have to find yours!!
-		int recVar ;
 		
-		for(Cookie c: kekse) {
-			if(c.getName().equals("test")) {
-				recVar = Integer.parseInt(c.getValue());  //the cookie value is always a String !!!
-			}
-		}
+		int sum, numb1, numb2;
 		
-		String contextValue;
+		numb1 = Integer.parseInt(req.getParameter("num1"));
+		numb2 = Integer.parseInt(req.getParameter("num2"));
 		
-		ServletConfig cg = getServletConfig();
-		contextValue = cg.getInitParameter("nameOfConfigParam");
+		sum = numb1 + numb2;
 		
 		
-		ServletContext ctx = getServletContext();
-		contextValue = ctx.getInitParameter("paramName01");
-		
-		String someVar = "yay";
+		String output = "the sum of the two numbers is: " + sum;
 		
 		PrintWriter out = res.getWriter();
-		out.print("Here can your wirte what ever shall appear on the page" + someVar);
+		out.print(output);
 		
 	}
 	
